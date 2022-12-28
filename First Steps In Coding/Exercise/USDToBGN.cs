b06dc61bdc966231edbf2bfc99e5e811b1c5f81d
Emil <emoemo2535@gmail.com>
2022-12-28 14:58:56 +0200

    Create USDToBGN.cs

---
+++ First Steps In Coding/Exercise/USDToBGN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace USDToBGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+
+            double toBgn = usd * 1.79549;
+
+            Console.WriteLine(toBgn);
+        }
+    }
+}
